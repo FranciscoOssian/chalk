8407f3da56ced43df086c1343b50d83d4611aec0
FranciscoOssian <ossian.neto@gmail.com>
2024-02-01 11:34:58 -0300

    fix/tests

--- src/services/i18n/i18n.test.js
+++ src/services/i18n/i18n.test.js
@@ -1,18 +1,17 @@
 import fabric from './index.js';
 
-// Mock de AsyncStorage
-jest.mock('@react-native-async-storage/async-storage', () => ({
-  getItem: () => 'en',
+// Mock de localStorage
+jest.mock('../localStorage', () => ({
+  __esModule: true,
+  default: jest.fn(() => ({
+    get: jest.fn(() => 'en'),
+  })),
 }));
 
 // Mock de i18n.use
 jest.mock('i18next', () => ({
   use: jest.fn().mockReturnThis(),
   init: jest.fn(),
-}));
-
-jest.mock('@utils/defaultStorage.ts', () => ({
-  defaultAppLanguage: 'en',
 }));
 
 // Mock de initReactI18next
